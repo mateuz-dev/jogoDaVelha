84965c6dc7937d5a900761b461e81c7f28355233
luuh-oliveira <lu.oliveira0505@gmail.com>
2021-11-03 11:19:16 -0300

    ajustes

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 const buttonComp = document.getElementById("vsComputador")
 
 function mostrarDificuldade() {
-    const dificuldade = document.getElementById("dificuldade")
-    dificuldade.style.display = "flex"
+    document.getElementById("dificuldade").style.display = "flex"
 }
 
 buttonComp.addEventListener("click", mostrarDificuldade())
